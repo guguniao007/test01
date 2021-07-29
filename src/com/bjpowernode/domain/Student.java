853c71e85eb5159a5eb0960c694e2bae6d0a5e66
qgugu <1006612475@qq.com>
2021-07-29 14:41:53 +0800

    1

---
+++ src/com/bjpowernode/domain/Student.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: llgugubird
+ * @Date: 2021/07/29/14:41
+ * @Description:
+ */
+public class Student {
+    private String id;
+}
