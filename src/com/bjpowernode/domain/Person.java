9956bf937f2137be7ad14957042e299ef12e9bf9
qgugu <1006612475@qq.com>
2021-07-29 15:18:48 +0800

    1

--- src/com/bjpowernode/domain/Person.java
+++ src/com/bjpowernode/domain/Person.java
@@ -1,12 +1,6 @@
 package com.bjpowernode.domain;
 
-/**
- * Created with IntelliJ IDEA.
- *
- * @Author: yoyo
- * @Date: 2021/07/29/11:33
- * @Description:
- */
+
 public class Person {
     private String id;
     private String name;
