8f764d3411730f729b68dbac1a69df0304dc56cb
zs <zs@bjowernode.com>
2021-07-29 12:07:47 +0800

    123

---
+++ src/com/bjpowernode/domain/Person.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: yoyo
+ * @Date: 2021/07/29/11:33
+ * @Description:
+ */
+public class Person {
+    private String id;
+
+}
